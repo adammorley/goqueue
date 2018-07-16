48b9a6b829867969a591a0c84bdde18614b74bb9
Adam Morley <adam@Adams-Mac-mini.local>
2018-07-15 17:14:03 -0700

    x

--- queue.go
+++ queue.go
@@ -1,7 +1,9 @@
-// a queue implementation for integers in golang
+// a queue implementation for values (using interface{} type to allow strings or integers or whatever) in golang
+// note that the Enqueue method enforces that each value added is of the same type, as a safety measure
 package queue
 
 import "errors"
+import "reflect"
 
 // a queue, which has a pointer to the front and back.  elements are added ("enqueue"'d) to the back and removed ("dequeue"'d) from the front
 type Queue struct {
@@ -13,7 +15,7 @@ type Queue struct {
 type element struct {
     next *element
     prev *element
-    value int
+    value interface{}
 }
 
 // a new queue, front and back pointers are nil
@@ -24,23 +26,26 @@ func New() *Queue {
     return q
 }
 
-// enqueue a value on the queue
-func (q *Queue) Enqueue(v int) {
+// enqueue a value on the queue, ensuring that the type of the to-be-added element is the same as the existing elements on the queue.
+func (q *Queue) Enqueue(i interface{}) {
     var e *element = new(element)
     e.next = nil
-    e.value = v
+    e.value = i
     e.prev = q.back
     if q.front == nil {
         q.front = e
         q.back = e
     } else {
+        if reflect.TypeOf(i) != reflect.TypeOf(q.front.value) {
+            panic("type mismatch")
+        }
         q.back.next = e
         q.back = e
     }
 }
 
 // dequeue a value on the queue.  returns error if queue empty
-func (q *Queue) Dequeue() (int, error) {
+func (q *Queue) Dequeue() (interface{}, error) {
     if q.front == nil {
         return 0, errors.New("empty queue")
     }
