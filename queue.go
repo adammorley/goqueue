c976c1f74148fa052ccab121770abae83e327fd9
Adam Morley <adam@Adams-Mac-mini.local>
2018-07-28 16:38:30 -0700

    add size/capacity functionality

--- queue.go
+++ queue.go
@@ -1,4 +1,4 @@
-// a queue implementation for values (using interface{} type to allow strings or integers or whatever) in golang
+// a queue implementation for values (using interface{} type to allow strings or integers or whatever) in golang.  allows for fixed size queues, and keeps track of the count.  this can be used as a ring buffer
 // note that the Enqueue method enforces that each value added is of the same type, as a safety measure
 package queue
 
@@ -9,6 +9,8 @@ import "reflect"
 type Queue struct {
     front *element
     back *element
+    size uint
+    capacity uint
 }
 
 // an element on the queue; each element has a pointer to the next and previous elements in the queue, along with the value stored at the element
@@ -19,15 +21,20 @@ type element struct {
 }
 
 // a new queue, front and back pointers are nil
-func New() *Queue {
+func New(capacity uint) *Queue {
     var q *Queue = new(Queue)
     q.front = nil
     q.back = nil
+    q.capacity = capacity
+    q.size = 0
     return q
 }
 
 // enqueue a value on the queue, ensuring that the type of the to-be-added element is the same as the existing elements on the queue.
-func (q *Queue) Enqueue(i interface{}) {
+func (q *Queue) Enqueue(i interface{}) error {
+    if q.size + 1 > q.capacity {
+        return errors.New("queue full")
+    }
     var e *element = new(element)
     e.next = nil
     e.value = i
@@ -37,29 +44,27 @@ func (q *Queue) Enqueue(i interface{}) {
         q.back = e
     } else {
         if reflect.TypeOf(i) != reflect.TypeOf(q.front.value) {
-            panic("type mismatch")
+            return errors.New("type mismatch")
         }
         q.back.next = e
         q.back = e
     }
+    q.size += 1
+    return nil
 }
 
 // dequeue a value on the queue.  returns error if queue empty
 func (q *Queue) Dequeue() (interface{}, error) {
     if q.front == nil {
-        return 0, errors.New("empty queue")
+        return nil, errors.New("empty queue")
     }
     var e *element = q.front
     q.front = e.next
+    q.size -= 1
     return e.value, nil
 }
 
 // size of the queue, in number of elements
-func (q *Queue) Size() (c uint) {
-    var e *element = q.front
-    for e != nil {
-        c++
-        e = e.next
-    }
-    return
+func (q *Queue) Size() uint {
+    return q.size
 }
--- queue_test.go
+++ queue_test.go
@@ -3,29 +3,41 @@ package queue
 import "testing"
 
 func TestQueue(t *testing.T) {
-    var q *Queue = New()
+    var q *Queue = New(128)
     if q.Size() != 0 {
         t.Error("new queue is not right sized")
     }
-    q.Enqueue(1)
-    if q.Size() != 1 {
+    if q.Enqueue(1) != nil {
+        t.Error("error on enqueue when not expected")
+    } else if q.Size() != 1 {
         t.Error("wrong size")
     }
-    q.Enqueue(1)
-    if q.Size() != 2 {
-        t.Error("wrong size")
-    }
-    q.Enqueue(2)
-    if q.Size() != 3 {
+    if q.Enqueue(1) != nil {
+        t.Error("error on enqueue when not expected")
+    } else if q.Size() != 2 {
         t.Error("wrong size")
     }
     if v, e := q.Dequeue(); e != nil || v != 1 {
         t.Error("dequeue not working")
     } else if v, e := q.Dequeue(); e != nil || v != 1 {
         t.Error("dequeue not working")
-    } else if v, e := q.Dequeue(); e != nil || v != 2 {
-        t.Error("dequeue not working")
-    } else if v, e := q.Dequeue(); e == nil || v != 0 {
+    } else if v, e := q.Dequeue(); e == nil || v != nil {
         t.Error("dequeue not detecting empty queue")
+    }
+
+    j := 3
+    var q1 *Queue = New(uint(j))
+    for i := 0; i < j; i++ {
+        q1.Enqueue(1)
+    }
+    if q1.Enqueue(1) == nil {
+        t.Error("expected error about full queue")
+    }
+
+    var q2 *Queue = New(4)
+    q2.Enqueue(2)
+    q2.Enqueue(-2)
+    if q2.Enqueue(4.3) == nil {
+        t.Error("expected error")
     }
 }
