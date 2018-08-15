fb6a92d5574e00487f496ceeff7ba75e359059b9
Adam Morley <adam@Adams-Mac-mini.local>
2018-08-15 09:46:02 -0700

    gofmt

--- queue.go
+++ queue.go
@@ -7,81 +7,83 @@ import "reflect"
 
 // a queue, which has a pointer to the front and back.  elements are added ("enqueue"'d) to the back and removed ("dequeue"'d) from the front
 type Queue struct {
-    front *element
-    back *element
-    size uint
-    capacity uint
+	front    *element
+	back     *element
+	size     uint
+	capacity uint
 }
 
 // a queue can return an error message in exceptional conditions
 type QueueError struct {
-    msg string
+	msg string
 }
+
 func (e *QueueError) Error() string {
-    return fmt.Sprintf("queue %v", e.msg)
+	return fmt.Sprintf("queue %v", e.msg)
 }
 
 // an element on the queue; each element has a pointer to the next and previous elements in the queue, along with the value stored at the element
 type element struct {
-    next *element
-    prev *element
-    value interface{}
+	next  *element
+	prev  *element
+	value interface{}
 }
 
 // a new queue, front and back pointers are nil
 func New(capacity uint) *Queue {
-    var q *Queue = new(Queue)
-    q.front = nil
-    q.back = nil
-    q.capacity = capacity
-    q.size = 0
-    return q
+	var q *Queue = new(Queue)
+	q.front = nil
+	q.back = nil
+	q.capacity = capacity
+	q.size = 0
+	return q
 }
 
 // since a queue can contain any arbitrary type, it can return an error if the type is not the same.
 type EnqueueTypeError struct {
-    passed string
-    stored string
+	passed string
+	stored string
 }
+
 func (e *EnqueueTypeError) Error() string {
-    return fmt.Sprintf("type mismatch, received: %v, expected: %v", e.passed, e.stored)
+	return fmt.Sprintf("type mismatch, received: %v, expected: %v", e.passed, e.stored)
 }
 
 // enqueue a value on the queue, ensuring that the type of the to-be-added element is the same as the existing elements on the queue.
 func (q *Queue) Enqueue(i interface{}) error {
-    if q.size + 1 > q.capacity {
-        return &QueueError{msg: "full"}
-    }
-    var e *element = new(element)
-    e.next = nil
-    e.value = i
-    e.prev = q.back
-    if q.front == nil {
-        q.front = e
-        q.back = e
-    } else {
-        if reflect.TypeOf(i) != reflect.TypeOf(q.front.value) {
-            return &EnqueueTypeError{passed: reflect.TypeOf(i).Name(), stored: reflect.TypeOf(q.front.value).Name()}
-        }
-        q.back.next = e
-        q.back = e
-    }
-    q.size += 1
-    return nil
+	if q.size+1 > q.capacity {
+		return &QueueError{msg: "full"}
+	}
+	var e *element = new(element)
+	e.next = nil
+	e.value = i
+	e.prev = q.back
+	if q.front == nil {
+		q.front = e
+		q.back = e
+	} else {
+		if reflect.TypeOf(i) != reflect.TypeOf(q.front.value) {
+			return &EnqueueTypeError{passed: reflect.TypeOf(i).Name(), stored: reflect.TypeOf(q.front.value).Name()}
+		}
+		q.back.next = e
+		q.back = e
+	}
+	q.size += 1
+	return nil
 }
 
 // dequeue a value on the queue.  returns error if queue empty
 func (q *Queue) Dequeue() (interface{}, error) {
-    if q.front == nil {
-        return nil, &QueueError{msg: "empty"}
-    }
-    var e *element = q.front
-    q.front = e.next
-    q.size -= 1
-    return e.value, nil
+	if q.front == nil {
+		return nil, &QueueError{msg: "empty"}
+	}
+	var e *element = q.front
+	q.front = e.next
+	q.size -= 1
+	return e.value, nil
 }
 
 // size of the queue, in number of elements
 func (q *Queue) Size() uint {
-    return q.size
+	return q.size
 }
--- queue_test.go
+++ queue_test.go
@@ -3,51 +3,51 @@ package queue
 import "testing"
 
 func TestQueue(t *testing.T) {
-    var q *Queue = New(128)
-    if q.Size() != 0 {
-        t.Error("new queue is not right sized")
-    }
-    if q.Enqueue(1) != nil {
-        t.Error("error on enqueue when not expected")
-    } else if q.Size() != 1 {
-        t.Error("wrong size")
-    }
-    if q.Enqueue(1) != nil {
-        t.Error("error on enqueue when not expected")
-    } else if q.Size() != 2 {
-        t.Error("wrong size")
-    }
-    if v, e := q.Dequeue(); e != nil || v != 1 {
-        t.Error("dequeue not working")
-    } else if v, e := q.Dequeue(); e != nil || v != 1 {
-        t.Error("dequeue not working")
-    } else if v, e := q.Dequeue(); e == nil || v != nil {
-        t.Error("dequeue not detecting empty queue")
-    }
-    _, e := q.Dequeue()
-    if _, ok := e.(*QueueError); !ok {
-        t.Error("type returned from dequeue incorrect", e)
-    }
+	var q *Queue = New(128)
+	if q.Size() != 0 {
+		t.Error("new queue is not right sized")
+	}
+	if q.Enqueue(1) != nil {
+		t.Error("error on enqueue when not expected")
+	} else if q.Size() != 1 {
+		t.Error("wrong size")
+	}
+	if q.Enqueue(1) != nil {
+		t.Error("error on enqueue when not expected")
+	} else if q.Size() != 2 {
+		t.Error("wrong size")
+	}
+	if v, e := q.Dequeue(); e != nil || v != 1 {
+		t.Error("dequeue not working")
+	} else if v, e := q.Dequeue(); e != nil || v != 1 {
+		t.Error("dequeue not working")
+	} else if v, e := q.Dequeue(); e == nil || v != nil {
+		t.Error("dequeue not detecting empty queue")
+	}
+	_, e := q.Dequeue()
+	if _, ok := e.(*QueueError); !ok {
+		t.Error("type returned from dequeue incorrect", e)
+	}
 
-    j := 3
-    var q1 *Queue = New(uint(j))
-    for i := 0; i < j; i++ {
-        q1.Enqueue(1)
-    }
-    if q1.Enqueue(1) == nil {
-        t.Error("expected error about full queue")
-    }
-    e = q1.Enqueue(1)
-    if _, ok := e.(*QueueError); !ok {
-        t.Error("type returned from enqueue incorrect", e)
-    }
+	j := 3
+	var q1 *Queue = New(uint(j))
+	for i := 0; i < j; i++ {
+		q1.Enqueue(1)
+	}
+	if q1.Enqueue(1) == nil {
+		t.Error("expected error about full queue")
+	}
+	e = q1.Enqueue(1)
+	if _, ok := e.(*QueueError); !ok {
+		t.Error("type returned from enqueue incorrect", e)
+	}
 
-    var q2 *Queue = New(4)
-    q2.Enqueue(2)
-    q2.Enqueue(-2)
-    e = q2.Enqueue(4.3)
-    // this means: if the error returned is not of the error type defined in the file, then it's an error.  golang syntax, ftw
-    if _, ok := e.(*EnqueueTypeError); !ok {
-        t.Error("expected error")
-    }
+	var q2 *Queue = New(4)
+	q2.Enqueue(2)
+	q2.Enqueue(-2)
+	e = q2.Enqueue(4.3)
+	// this means: if the error returned is not of the error type defined in the file, then it's an error.  golang syntax, ftw
+	if _, ok := e.(*EnqueueTypeError); !ok {
+		t.Error("expected error")
+	}
 }
